14c14dda7520db4d90297606dd651c72c13600b2
Niklas Widmann <nik@somethingcatchy.net>
2024-11-06 17:45:08 +0100

    slabs & stairs

--- src/main/kotlin/com/possible_triangle/dye_the_world/index/DyedClayworks.kt
+++ src/main/kotlin/com/possible_triangle/dye_the_world/index/DyedClayworks.kt
@@ -2,14 +2,9 @@ package com.possible_triangle.dye_the_world.index
 
 import com.possible_triangle.dye_the_world.*
 import com.possible_triangle.dye_the_world.ForgeEntrypoint.REGISTRATE
-import com.possible_triangle.dye_the_world.data.slabRecipes
-import com.possible_triangle.dye_the_world.data.stairRecipes
 import net.minecraft.tags.BlockTags
-import net.minecraft.tags.ItemTags
 import net.minecraft.world.item.CreativeModeTabs
 import net.minecraft.world.level.block.Block
-import net.minecraft.world.level.block.SlabBlock
-import net.minecraft.world.level.block.StairBlock
 
 object DyedClayworks {
 
@@ -36,14 +31,25 @@ object DyedClayworks {
 
     val TERRACOTTA_BRICKS_SLABS = REGISTRATE.createSlabs(
         TERRACOTTA_BRICKS,
-        "terracotta_bricks",
-        { Constants.MOD_ID.createId("block/clayworks/${it}_terracotta_bricks") }
+        "terracotta_brick",
+        modifyBlock = { dye ->
+            blockstate { c, p ->
+                val texture = Constants.MOD_ID.createId("block/clayworks/${dye}_terracotta_bricks")
+                val double = Constants.MOD_ID.createId("block/${dye}_terracotta_bricks")
+                p.slabBlock(c.get(), double, texture)
+            }
+        }
     )
 
     val TERRACOTTA_BRICKS_STAIRS = REGISTRATE.createStairs(
         TERRACOTTA_BRICKS,
-        "terracotta_bricks",
-        { Constants.MOD_ID.createId("block/clayworks/${it}_terracotta_bricks") }
+        "terracotta_brick",
+        modifyBlock = { dye ->
+            blockstate { c, p ->
+                val texture = Constants.MOD_ID.createId("block/clayworks/${dye}_terracotta_bricks")
+                p.stairsBlock(c.get(), texture)
+            }
+        }
     )
 
     val CHISELED_TERRACOTTA_BRICKS = DYES.associateWith { dye ->
@@ -67,49 +73,9 @@ object DyedClayworks {
 
     private val TERRACOTTA = dyedBlockMap("terracotta")
 
-    val TERRACOTTA_SLABS = DYES.associateWith { dye ->
-        val base = { dye.blockOf("terracotta") }
+    val TERRACOTTA_SLABS = REGISTRATE.createSlabs(TERRACOTTA, "terracotta")
 
-        REGISTRATE.`object`("${dye}_terracotta_slab")
-            .block(::SlabBlock)
-            .initialProperties(base)
-            .tag(BlockTags.MINEABLE_WITH_PICKAXE)
-            .tag(BlockTags.SLABS)
-            .blockstate { c, p ->
-                val texture = Constants.Mods.DYE_DEPOT.createId("block/${dye}_terracotta")
-                p.slabBlock(c.get(), texture, texture)
-            }
-            .lang("${dye.translation} Terracotta Slab")
-            .withItem {
-                tab(CreativeModeTabs.COLORED_BLOCKS)
-                tab(CreativeModeTabs.BUILDING_BLOCKS)
-                tag(ItemTags.SLABS)
-                slabRecipes(base)
-            }
-            .register()
-    }
-
-    val TERRACOTTA_STAIRS = DYES.associateWith { dye ->
-        val base = { dye.blockOf("terracotta") }
-
-        REGISTRATE.`object`("${dye}_terracotta_stairs")
-            .block { StairBlock({ base().defaultBlockState() }, it) }
-            .initialProperties(base)
-            .tag(BlockTags.MINEABLE_WITH_PICKAXE)
-            .tag(BlockTags.STAIRS)
-            .blockstate { c, p ->
-                val texture = Constants.Mods.DYE_DEPOT.createId("block/${dye}_terracotta")
-                p.stairsBlock(c.get(), texture)
-            }
-            .lang("${dye.translation} Terracotta Stairs")
-            .withItem {
-                tag(ItemTags.STAIRS)
-                tab(CreativeModeTabs.COLORED_BLOCKS)
-                tab(CreativeModeTabs.BUILDING_BLOCKS)
-                stairRecipes(base)
-            }
-            .register()
-    }
+    val TERRACOTTA_STAIRS = REGISTRATE.createStairs(TERRACOTTA, "terracotta")
 
     fun register() {
         // Loads this class
--- src/main/kotlin/com/possible_triangle/dye_the_world/index/DyedSlabs.kt
+++ src/main/kotlin/com/possible_triangle/dye_the_world/index/DyedSlabs.kt
@@ -3,6 +3,7 @@ package com.possible_triangle.dye_the_world.index
 import com.possible_triangle.dye_the_world.DyedRegistrate
 import com.possible_triangle.dye_the_world.createId
 import com.possible_triangle.dye_the_world.data.slabRecipes
+import com.possible_triangle.dye_the_world.data.stairRecipes
 import com.possible_triangle.dye_the_world.namespace
 import com.possible_triangle.dye_the_world.withItem
 import com.tterrag.registrate.builders.BlockBuilder
@@ -21,7 +22,6 @@ import net.minecraft.world.level.block.StairBlock
 fun DyedRegistrate.createSlabs(
     from: Map<DyeColor, NonNullSupplier<Block>>,
     name: String,
-    texture: (DyeColor) -> ResourceLocation = { it.namespace.createId("block/${it}_${name}") },
     modifyBlock: BlockBuilder<SlabBlock, DyedRegistrate>.(DyeColor) -> Unit = {},
     modifyItem: ItemBuilder<BlockItem, BlockBuilder<SlabBlock, DyedRegistrate>>.(DyeColor) -> Unit = {},
 ) = from.mapValues { (dye, base) ->
@@ -31,7 +31,8 @@ fun DyedRegistrate.createSlabs(
         .tag(BlockTags.MINEABLE_WITH_PICKAXE)
         .tag(BlockTags.SLABS)
         .blockstate { c, p ->
-            p.slabBlock(c.get(), texture(dye), texture(dye))
+            val texture = dye.namespace.createId("block/${dye}_${name}")
+            p.slabBlock(c.get(), dye.namespace.createId("block/${dye}_${name}"), texture)
         }
         .withItem {
             tab(CreativeModeTabs.COLORED_BLOCKS)
@@ -47,23 +48,25 @@ fun DyedRegistrate.createSlabs(
 fun DyedRegistrate.createStairs(
     from: Map<DyeColor, NonNullSupplier<Block>>,
     name: String,
-    texture: (DyeColor) -> ResourceLocation = { it.namespace.createId("block/${it}_${name}") },
-    block: BlockBuilder<StairBlock, DyedRegistrate>.() -> Unit = {}
+    modifyBlock: BlockBuilder<StairBlock, DyedRegistrate>.(DyeColor) -> Unit = {},
+    modifyItem: ItemBuilder<BlockItem, BlockBuilder<StairBlock, DyedRegistrate>>.(DyeColor) -> Unit = {},
 ) = from.mapValues { (dye, base) ->
-    `object`("${dye}_${name}_slab")
+    `object`("${dye}_${name}_stairs")
         .block { StairBlock({ base.get().defaultBlockState() }, it) }
         .initialProperties(base)
         .tag(BlockTags.MINEABLE_WITH_PICKAXE)
         .tag(BlockTags.STAIRS)
         .blockstate { c, p ->
-            p.stairsBlock(c.get(), texture(dye))
+            val texture = dye.namespace.createId("block/${dye}_${name}")
+            p.stairsBlock(c.get(), texture)
         }
         .withItem {
             tab(CreativeModeTabs.COLORED_BLOCKS)
             tab(CreativeModeTabs.BUILDING_BLOCKS)
             tag(ItemTags.STAIRS)
-            slabRecipes { base.get() }
+            stairRecipes { base.get() }
+            modifyItem(dye)
         }
-        .apply(block)
+        .apply { modifyBlock(dye) }
         .register()
 }
