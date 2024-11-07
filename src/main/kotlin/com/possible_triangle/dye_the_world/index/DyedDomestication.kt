9eacd164b28526bb5da6d6b9253b3aede238e288
Niklas Widmann <nik@somethingcatchy.net>
2024-11-07 13:12:19 +0100

    move pet beds to correct creative tab

--- src/main/kotlin/com/possible_triangle/dye_the_world/index/DyedDomestication.kt
+++ src/main/kotlin/com/possible_triangle/dye_the_world/index/DyedDomestication.kt
@@ -8,12 +8,17 @@ import com.possible_triangle.dye_the_world.data.petBedRecipe
 import com.possible_triangle.dye_the_world.dyesFor
 import com.possible_triangle.dye_the_world.translation
 import com.possible_triangle.dye_the_world.withItem
+import net.minecraft.core.registries.Registries
+import net.minecraft.resources.ResourceKey
+import net.minecraft.resources.ResourceLocation
 import net.minecraft.tags.BlockTags
-import net.minecraft.world.item.CreativeModeTabs
 import net.minecraft.world.level.block.SoundType
 import net.minecraft.world.level.material.PushReaction
 
 object DyedDomestication {
+
+    private val TAB =
+        ResourceKey.create(Registries.CREATIVE_MODE_TAB, ResourceLocation(DOMESTICATION_INNOVATION, DOMESTICATION_INNOVATION))
 
     val PET_BEDS = dyesFor(DOMESTICATION_INNOVATION).associateWith { dye ->
         REGISTRATE.`object`("pet_bed_${dye}")
@@ -27,8 +32,7 @@ object DyedDomestication {
             .tag(BlockTags.MINEABLE_WITH_AXE)
             .petBedBlockstate()
             .withItem {
-                tab(CreativeModeTabs.COLORED_BLOCKS)
-                tab(CreativeModeTabs.FUNCTIONAL_BLOCKS)
+                tab(TAB)
                 tag(DyedTags.Items.PET_BEDS)
                 petBedRecipe(dye)
             }
