b18b8a5d8b5e19045813c3e6294bdf5a5c42bf37
Niklas Widmann <nik@somethingcatchy.net>
2024-11-08 16:11:27 +0100

    seats

--- src/main/kotlin/com/possible_triangle/dye_the_world/data/SeatGen.kt
+++ src/main/kotlin/com/possible_triangle/dye_the_world/data/SeatGen.kt
@@ -1,14 +1,18 @@
 package com.possible_triangle.dye_the_world.data
 
+import com.possible_triangle.dye_the_world.Constants
 import com.possible_triangle.dye_the_world.Constants.Mods.CREATE
 import com.possible_triangle.dye_the_world.createId
 import com.tterrag.registrate.builders.BlockBuilder
 import net.minecraft.world.item.DyeColor
 import net.minecraft.world.level.block.Block
 
-// TODO modify after creating texture
 fun <T : Block, P> BlockBuilder<T, P>.seatBlockstate(dye: DyeColor) = blockstate { context, provider ->
-    val parent = CREATE.createId("block/red_seat")
+    val parent = CREATE.createId("block/seat")
     val model = provider.models().withExistingParent(context.name, parent)
+        .texture("1", Constants.MOD_ID.createId("block/$CREATE/seat/top_$dye"))
+        .texture("2", Constants.MOD_ID.createId("block/$CREATE/seat/side_$dye"))
+
+
     provider.simpleBlock(context.get(), model)
 }
