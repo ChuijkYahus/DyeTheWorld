cfeed4dcf76a53d8efd029ad923445a1246bdcaf
Niklas Widmann <nik@somethingcatchy.net>
2025-03-10 14:41:53 +0100

    finish bedrolls

--- src/main/java/com/possible_triangle/dye_the_world/mixins/data/RegistrateRecipeProviderMixin.java
+++ src/main/java/com/possible_triangle/dye_the_world/mixins/data/RegistrateRecipeProviderMixin.java
@@ -6,44 +6,48 @@ import com.possible_triangle.dye_the_world.data.ConditionalFinishedRecipe;
 import com.possible_triangle.dye_the_world.data.DyedRegistrateRecipeProvider;
 import com.tterrag.registrate.providers.RegistrateRecipeProvider;
 import net.minecraft.data.recipes.FinishedRecipe;
+import net.minecraft.world.level.ItemLike;
 import net.minecraftforge.common.crafting.conditions.ModLoadedCondition;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 
+import java.util.ArrayDeque;
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
+import java.util.Queue;
+import java.util.stream.Stream;
 
 @Mixin(value = RegistrateRecipeProvider.class, remap = false)
-public class RegistrateRecipeProviderMixin implements DyedRegistrateRecipeProvider {
+public abstract class RegistrateRecipeProviderMixin implements DyedRegistrateRecipeProvider {
 
     @Unique
-    @Nullable
-    private String namespace = null;
+    private final Queue<String> namespaces = Collections.asLifoQueue(new ArrayDeque<>());
 
     @Override
-    public void setNamespace(String namespace) {
-        this.namespace = namespace;
+    public void pushNamespace(String namespace) {
+        namespaces.add(namespace);
     }
 
     @Override
-    public void resetNamespace() {
-        namespace = null;
+    public void popNamespace() {
+        namespaces.poll();
     }
 
     @Override
-    public @Nullable String getNamespace() {
-        return namespace;
+    public Stream<String> getNamespaces() {
+        return namespaces.stream().distinct();
     }
 
     @WrapMethod(
             method = "accept(Lnet/minecraft/data/recipes/FinishedRecipe;)V"
     )
     private void injectSave(FinishedRecipe recipe, Operation<Void> original) {
-        var conditions = Optional.ofNullable(namespace)
+        var conditions = getNamespaces()
                 .map(ModLoadedCondition::new)
-                .map(List::of)
-                .orElseGet(List::of);
+                .toList();
 
         original.call(new ConditionalFinishedRecipe(recipe, conditions));
     }
--- src/main/kotlin/com/possible_triangle/dye_the_world/index/DyedAquatic.kt
+++ src/main/kotlin/com/possible_triangle/dye_the_world/index/DyedAquatic.kt
@@ -1,26 +1,22 @@
 package com.possible_triangle.dye_the_world.index
 
+import com.possible_triangle.dye_the_world.*
 import com.possible_triangle.dye_the_world.Constants.Mods.UPGRADE_AQUATIC
 import com.possible_triangle.dye_the_world.ForgeEntrypoint.REGISTRATE
 import com.possible_triangle.dye_the_world.data.bedrollBlockstate
 import com.possible_triangle.dye_the_world.data.bedrollItemModel
+import com.possible_triangle.dye_the_world.data.bedrollLoot
 import com.possible_triangle.dye_the_world.data.bedrollRecipe
-import com.possible_triangle.dye_the_world.dyesFor
-import com.possible_triangle.dye_the_world.extensions.optionalTag
-import com.possible_triangle.dye_the_world.extensions.translation
-import com.possible_triangle.dye_the_world.extensions.withItem
+import com.possible_triangle.dye_the_world.extensions.*
 import com.teamabnormals.upgrade_aquatic.common.block.BedrollBlock
 import com.teamabnormals.upgrade_aquatic.core.other.tags.UABlockTags
 import com.teamabnormals.upgrade_aquatic.core.other.tags.UAItemTags
 import com.teamabnormals.upgrade_aquatic.core.registry.UABlocks
-import net.minecraft.core.registries.Registries
-import net.minecraft.resources.ResourceKey
-import net.minecraft.resources.ResourceLocation
+import com.tterrag.registrate.providers.ProviderType
+import net.minecraft.core.registries.BuiltInRegistries
+import net.minecraft.world.item.CreativeModeTabs
 
 object DyedAquatic {
-
-    private val TAB =
-        ResourceKey.create(Registries.CREATIVE_MODE_TAB, ResourceLocation(UPGRADE_AQUATIC, UPGRADE_AQUATIC))
 
     val PET_BEDS = dyesFor(UPGRADE_AQUATIC).associateWith { dye ->
         REGISTRATE.`object`("${dye}_bedroll")
@@ -29,14 +25,35 @@ object DyedAquatic {
             .properties { it.mapColor(dye) }
             .lang("${dye.translation} Bedroll")
             .bedrollBlockstate(dye)
+            .bedrollLoot()
             .optionalTag(UABlockTags.BEDROLLS)
             .withItem {
-                tab(TAB)
+                tab(CreativeModeTabs.FUNCTIONAL_BLOCKS)
+                tab(CreativeModeTabs.COLORED_BLOCKS)
                 optionalTag(UAItemTags.BEDROLLS)
                 bedrollRecipe(dye)
                 bedrollItemModel(dye)
             }
             .register()
+    }
+
+    init {
+        REGISTRATE.addDataGenerator(ProviderType.RECIPE) { provider ->
+            provider.withNamespace(UPGRADE_AQUATIC) {
+                provider.cleaningRecipe(
+                    BuiltInRegistries.ITEM.getOrThrow(UPGRADE_AQUATIC.createId("bedroll")),
+                    UAItemTags.BEDROLLS
+                )
+
+                VANILLA_DYES.forEach { dye ->
+                    provider.dyeingRecipe(dye, UAItemTags.BEDROLLS, {
+                        BuiltInRegistries.ITEM.getOrThrow(UPGRADE_AQUATIC.createId("${dye}_bedroll"))
+                    }, id = UPGRADE_AQUATIC.createId("dye_${dye}_bedroll")) {
+                        group("bedroll")
+                    }
+                }
+            }
+        }
     }
 
 
