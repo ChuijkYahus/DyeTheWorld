d3dc749ecde6d505c562b86b520108c5581d2059
Niklas Widmann <nik@somethingcatchy.net>
2025-03-11 17:00:01 +0100

    add concurrency to workflows

--- src/main/kotlin/com/possible_triangle/dye_the_world/compat/AlexsMobsCompat.kt
+++
@@ -1,9 +0,0 @@
-package com.possible_triangle.dye_the_world.compat
-
-import net.minecraft.world.item.DyeColor
-import net.minecraft.world.item.Item
-
-object AlexsMobsCompat {
-    fun getCarpet(color: DyeColor?): Item? {
-    }
-}
